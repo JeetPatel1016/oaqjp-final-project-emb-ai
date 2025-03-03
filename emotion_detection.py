5404f910ea84d63ecfbba95a2d28d2230b4dbaca
Jeet Patel <Jeetpatel1011@gmail.com>
2025-03-03 13:54:13 -0500

    Added emotion detection package

---
+++ emotion_detection.py
@@ -0,0 +1,19 @@
+import requests
+import json
+def emotion_detector(text_to_analyse):
+    URL = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    myObj = { "raw_document": { "text": text_to_analyse } }
+    headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    response = requests.post(URL, json=myObj, headers=headers)
+    formatted_response = json.loads(response.text)
+    scores = formatted_response['emotionPredictions'][0]['emotion']
+    result = scores.copy()
+    maxScore = 0
+    dominantEmotion = ""
+    for emotion in scores:
+        if(scores[emotion] > maxScore):
+            dominantEmotion = emotion
+            maxScore = scores[emotion]
+    result['dominant_emotion'] = dominantEmotion
+
+    return result
