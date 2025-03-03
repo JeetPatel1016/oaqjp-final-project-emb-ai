02a35d64fefaa47c5b51bdc6d7e5cb60fd03afd7
Jeet Patel <Jeetpatel1011@gmail.com>
2025-03-03 15:06:15 -0500

    Added server.py for deploying flask web app

---
+++ server.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, request
+from EmotionDetection.emotion_detection import emotion_detector
+
+app = Flask("Emotion Detector")
+
+@app.route('/')
+def render_homepage():
+    return render_template('index.html')
+
+@app.route('/emotionDetector')
+def get_emotion_results():
+    text_to_analyze = request.args.get('textToAnalyze')
+    print("Tecxt: ", text_to_analyze)
+    if text_to_analyze is None:
+        return "No text was provided."
+    
+    data = emotion_detector(text_to_analyze)
+    if data['dominant_emotion'] is None:
+        return "Error parsing the sentence. Please try later."
+    
+    return (
+        f"Based on the analysis, the detected emotion is {data['dominant_emotion']}. "
+        f"Here are the probability scores: "
+        f"Anger: {data['anger']:.2f}, "
+        f"Sadness: {data['sadness']:.2f}, "
+        f"Disgust: {data['disgust']:.2f}, "
+        f"Fear: {data['fear']:.2f}, "
+        f"Joy: {data['joy']:.2f}."
+    )
+if __name__ == "__main__":
+    app.run()
